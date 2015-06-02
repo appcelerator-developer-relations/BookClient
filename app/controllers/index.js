db3ca76132388574fce64859d9ee564a2cdd8e6e
Ben Hatfield <bhatfield@appcelerator.com>
2015-06-02 16:25:52 -0700

    [TIDOC-2211] Support BookService Arrow App

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,39 +3,24 @@ library.fetch();
 $.index.open();
 
 function showBook(e) {
-	var book = library.at(e.index);
-	var detail = Alloy.createController('detail', book).getView();
-	detail.open();
+	var detailWin = Alloy.createController('detail', library.at(e.index)).getView()
+	if (OS_ANDROID) {
+		detailWin.open();
+	} else if (OS_IOS) {
+		$.index.openWindow(detailWin);
+	}
 }
 
 function addBook(e) {
-	var add = Alloy.createController('add').getView();
-	add.open();
+	var addWin = Alloy.createController('add').getView()
+	if (OS_ANDROID) {
+		addWin.open();
+	} else if (OS_IOS) {
+		$.index.openWindow(addWin);
+	}
 }
 
 function refreshTable(e) {
 	library.fetch();
 }
 
-// Initializes the Android Action Bar
-// Bound to the window's open event
-function loadExtras (e) {
-	if (OS_ANDROID) {
-		$.index.activity.onCreateOptionsMenu = function(e) { 
-			var menu = e.menu; 
-			var addItem = menu.add({ 
-				title : "Add", 
-				icon : 'ic_menu_add.png',
-				showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS
-			}); 
-			addItem.addEventListener("click", addBook);
-
-			var refreshItem = menu.add({
-				title : "Refresh",
-				icon : 'ic_menu_refresh.png',
-				showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS
-			}); 
-			refreshItem.addEventListener("click", refreshTable);
-		};
-	}
-}
--- app/lib/alloy/sync/book_rest.js
+++ app/lib/alloy/sync/book_rest.js
@@ -1,7 +1,9 @@
 // This sync adapter makes HTTP requests to the BookService to manage data
 
-// Global URL variable
-var BASE_URL = 'http://localhost:8080/book/';
+// Global URL variables
+var BASE_URL,
+	API_KEY,
+	AUTH_TYPE;
 
 // Override the Backbone.sync method with the following
 module.exports.sync = function(method, model, options) {
@@ -17,20 +19,18 @@ module.exports.sync = function(method, model, options) {
 			if (payload[model.idAttribute]) {
 				// If we have an ID, fetch only one document
 				http_request('GET', BASE_URL + payload[model.idAttribute], null, callback);
-			}
-			else {
+			} else {
 				// if not, fetch all documents
 				http_request('GET', BASE_URL, null, callback);
 			}
 			break;
 
 		// This case is called by the Model.save and Collection.create methods
-		// to a initialize model if the IDs are not set.
+		// to initialize a model if the IDs are not set.
 		case 'create':
 			if (payload.title && payload.author) {
 				http_request('POST', BASE_URL, {title: payload.title, author: payload.author}, callback);
-			}
-			else {
+			} else {
 				error = 'ERROR: Cannot create model without an author or title!';
 			}
 			break;
@@ -39,8 +39,7 @@ module.exports.sync = function(method, model, options) {
 		case 'delete':
 			if (payload[model.idAttribute]) {
 				http_request('DELETE', BASE_URL + payload[model.idAttribute], null, callback);
-			}
-			else {
+			} else {
 				error = 'ERROR: Model does not have an ID!';
 			}
 			break;
@@ -50,8 +49,7 @@ module.exports.sync = function(method, model, options) {
 		case 'update':
 			if (payload[model.idAttribute]) {
 				http_request('PUT', BASE_URL + payload[model.idAttribute], {title: payload.title, author: payload.author}, callback);
-			}
-			else {
+			} else {
 				error = 'ERROR: Model does not have an ID!';
 			}
 			break;
@@ -67,52 +65,72 @@ module.exports.sync = function(method, model, options) {
 	}
 
 	// Simple default callback function for HTTP request operations.
-	function callback(success, response, error) {
-		res = JSON.parse(response);
-		if (success) {
-			// Calls the default Backbone success callback
-			// and invokes a custom callback if options.success was defined.
-			options.success(res, JSON.stringify(res), options);
-		}
-		else {
+	function callback(error, response) {
+		var res = response ? JSON.parse(response) : null;
+		if (error) {
 			// Calls the default Backbone error callback
 			// and invokes a custom callback if options.error was defined.
 			var err = res.error || error;
 			Ti.API.error('ERROR: ' + err);
 			options.error(model, error, options);
 			model.trigger('error');
+		} else {
+			// Calls the default Backbone success callback
+			// and invokes a custom callback if options.success was defined.
+			if (res && res.key) {
+				// Get only the model(s)
+				res = res[res.key];
+			}
+			options.success(res, response, options);
 		}
 	};
 };
 
 // Helper function for creating an HTTP request
 function http_request(method, url, payload, callback) {
-
 	var client = Ti.Network.createHTTPClient({
 		onload: function(e) {
-			if (callback) callback(true, this.responseText, null);
+			if (callback) {
+				var resource = this.getResponseHeader('Location') || null;
+				if (resource) {
+					// Arrow applications do not return a payload response for non-GET methods.
+					// Need to retrieve the model to pass to Backbone APIs
+					resource = resource.slice(resource.lastIndexOf('/') + 1);
+					http_request('GET', BASE_URL + resource, null, callback);
+				} else {
+					callback(null, this.responseText);
+				}
+			}
 		},
 		onerror: function(e) {
-			if (callback) callback(false, this.responseText, e.error);
+			if (callback) {
+				callback(e.error, this.responseText);
+			}
 		},
 		timeout : 5000
 	});
 
 	client.open(method, url);
+	if (AUTH_TYPE === 'basic') {
+		client.setRequestHeader('Authorization', 'Basic ' + Ti.Utils.base64encode(API_KEY + ':'));
+	}
+	if (AUTH_TYPE === 'apikey') {
+		client.setRequestHeader('APIKey', API_KEY);
+	}
 	client.send(payload);
 };
 
 // Perform some actions before creating the Model class
 module.exports.beforeModelCreate = function(config, name) {
 	config = config || {};
-	// If there is a base_url defined in the model file, use it
-	if (config.adapter.base_url) {
-		BASE_URL = config.adapter.base_url;
-	}
+	// Set configuration settings
+	BASE_URL = config.adapter.base_url || 'http://localhost:8080/api/book';
+	API_KEY = config.adapter.api_key || '';
+	AUTH_TYPE = config.adapter.auth_type || 'basic';
 	return config;
 };
 
-// Perform some actions after creating the Model class 
+// Perform some actions after creating the Model class
 module.exports.afterModelCreate = function(Model, name) {
 	// Nothing to do
 };
